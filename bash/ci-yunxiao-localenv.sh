af785f013d26ecd636e944929ff542c6846364c5
baijie <baijie123>
2023-08-30 14:07:55 +0800

    fea():增加阿里云-云效 环境打包脚本

--- bash/ci-yunxiao-localenv.sh
+++ bash/ci-yunxiao-localenv.sh
@@ -38,5 +38,5 @@ cd /root/workspace/fast-quarkus_DbBc
 export JAVA_HOME=/root/opt/graalvm
 export PATH=$PATH:$JAVA_HOME/bin
 /root/opt/apache-maven-3.8.6/bin/mvn  -v
-/root/opt/apache-maven-3.8.6/bin/mvn -s /root/.m2/settings.xml clean package -Pnative
+/root/opt/apache-maven-3.8.6/bin/mvn -s /root/.m2/settings.xml -DskipTests=true clean package  -Pnative
 
