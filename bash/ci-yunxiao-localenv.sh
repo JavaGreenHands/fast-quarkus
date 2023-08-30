b507d4e5ad520017dbf67f200e3ead158b446194
baijie <baijie123>
2023-08-30 11:11:11 +0800

    fea():增加阿里云-云效 环境打包脚本

--- bash/ci-yunxiao-localenv.sh
+++ bash/ci-yunxiao-localenv.sh
@@ -11,13 +11,13 @@ if [ ! -f "graalvm-jdk-20_linux-x64_bin.tar.gz" ]; then
 fi
 
 # 解压 graalvm
-if [ ! -d "/root/localenv/graalvm" ]; then
-    mkdir -p /root/localenv/graalvm
-    tar -xf graalvm-jdk-20_linux-x64_bin.tar.gz -C /root/localenv/graalvm --strip-components=1
+if [ ! -d "/root/opt/graalvm" ]; then
+    mkdir -p /root/opt/graalvm
+    tar -xf graalvm-jdk-20_linux-x64_bin.tar.gz -C /root/opt/graalvm --strip-components=1
 fi
 
 # 设置 graalvm_home 环境变量
-export GRAALVM_HOME=/root/localenv/graalvm
+export GRAALVM_HOME=/root/opt/graalvm
 export PATH=$PATH:$GRAALVM_HOME/bin
 # 判断 apache-maven-3.8.6-bin.tar.gz 是否存在，如果不存在则下载
 if [ ! -f "apache-maven-3.8.6-bin.tar.gz" ]; then
@@ -26,9 +26,9 @@ if [ ! -f "apache-maven-3.8.6-bin.tar.gz" ]; then
 fi
 
 # 解压 apache-maven
-if [ ! -d "/root/localenv/apache-maven-3.8.6" ]; then
-    mkdir -p /root/localenv/apache-maven-3.8.6
-    tar -xf apache-maven-3.8.6-bin.tar.gz -C /root/localenv/apache-maven-3.8.6 --strip-components=1
+if [ ! -d "/root/opt/apache-maven-3.8.6" ]; then
+    mkdir -p /root/opt/apache-maven-3.8.6
+    tar -xf apache-maven-3.8.6-bin.tar.gz -C /root/opt/apache-maven-3.8.6 --strip-components=1
 fi
 
 # 输出提示信息
@@ -38,6 +38,6 @@ echo "GraalVM and Apache Maven are ready!"
 cd /root/workspace/fast-quarkus_DbBc
 export JAVA_HOME=/root/workspace/fast-quarkus_DbBc/graalvm-jdk-20.0.2+9.1/
 export PATH=$PATH:$JAVA_HOME/bin
-/root/localenv/apache-maven-3.8.6/mvn  -v
-/root/localenv/apache-maven-3.8.6/mvn -s /root/.m2/settings.xml clean package -Pnative -DJAVA_HOME=/root/workspace/fast-quarkus_DbBc/graalvm-jdk-20.0.2+9.1/bin
+/root/opt/apache-maven-3.8.6/mvn  -v
+/root/opt/apache-maven-3.8.6/mvn -s /root/.m2/settings.xml clean package -Pnative
 
